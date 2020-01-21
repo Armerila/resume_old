a819543983cb74992c6205dc12c882427f425547
Armerila <arkkuvio@gmail.com>
2020-01-21 13:44:46 +0200

    Work on content

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,27 +5,21 @@ class Home extends React.Component {
         return (
             <div className='home-div'>
                 <div className='home-p'>
-                    <p><b>I Am A:</b> Neutral Good Human Bard (3rd Level)</p>
-                    <p><u>Ability Scores:</u></p>
-                    <p><b>Strength-</b>11</p>
-                    <p><b>Dexterity-</b>13</p>
-                    <p><b>Constitution-</b>13</p>
-                    <p><b>Intelligence-</b>13</p>
-                    <p><b>Wisdom-</b>16</p>
-                    <p><b>Charisma-</b>13</p>
-                    <p><u>Alignment: </u><b>Neutral Good</b></p>
-                    <p>A neutral good character does the best that a good person can do. He is devoted to helping others. He works with kings and magistrates but does not feel beholden to them. Neutral good is the best alignment you can be because it means doing what is good without bias for or against order. However, neutral good can be a dangerous alignment when it advances mediocrity by limiting the actions of the truly capable. </p>
-                    <u>Race: </u><b>Humans</b> 
-                    <p>are the most adaptable of the common races. Short generations and a penchant for migration and conquest have made them physically diverse as well. Humans are often unorthodox in their dress, sporting unusual hairstyles, fanciful clothes, tattoos, and the like.</p>
-                    <u>Class: </u><b>Bards</b>
-                    <p>often serve as negotiators, messengers, scouts, and spies. They love to accompany heroes (and villains) to witness heroic (or villainous) deeds firsthand, since a bard who can tell a story from personal experience earns renown among his fellows. A bard casts arcane spells without any advance preparation, much like a sorcerer. Bards also share some specialized skills with rogues, and their knowledge of item lore is nearly unmatched. A high Charisma score allows a bard to cast high-level spells.</p>  
+                    <h3 className= 'tabbed'>Koulutus</h3>
+                    <p className='tabbed'>
+                        <b>Filosofian maisteri, Computational Big Data Analytics | 2019 -</b>
+                        <b>Luonnontieteiden kandidaatti, tietojenkäsittelytiede | 2015 - 2019</b>
+                        <img src={require('../assets/uni_oulu.png')} className='home'></img><br></br>
+                    <b>Ylioppilas | 2011 - 2014</b><br></br>
+                    Madetojan musiikkilukio, Oulu</p>
+                    <h3>Työkokemus</h3>
                     <h3>Kielitaito:</h3>
                     <p><b>Suomi -</b> Äidinkieli <br></br>
-                    <b>Englanti -</b> Erinomainen <br></br>
-                    <b>Ruotsi -</b> Perusteet</p>
-                </div> 
-            </div>    
-        )    
+                        <b>Englanti -</b> Erinomainen <br></br>
+                        <b>Ruotsi -</b> Perusteet</p>
+                </div>
+            </div>
+        )
     }
 }
 export default Home;
