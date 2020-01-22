f1f7a94aaa2c5aad44579b8be876cf991bf1c60b
Arttu Meriläinen <arkkuvio@gmail.com>
2020-01-22 20:30:23 +0200

    Scaled pdf export to A4

--- src/App.js
+++ src/App.js
@@ -8,18 +8,22 @@ import Home from './components/Home.js'
 class App extends React.Component {
   pdfExportComponent;
   render() {
-  return (
-    <PDFExport ref={(component) => this.pdfExportComponent = component} margin={{ top: 20, left: 100, right: 30, bottom: 20 }}>
-      <div className='root'>
-        <Home></Home><button className="load-button" onClick={this.exportPDFWithComponent}>Lataa PDF-muodossa</button>
-        <Sidebar></Sidebar>
-      </div>
+    return (
+      <PDFExport ref={(component) => this.pdfExportComponent = component}
+        margin={{ top: 20, left: 0, right: 10, bottom: 20 }}
+        fileName="CV_Arttu_Merilainen.pdf"
+        paperSize= {"A4"}
+        scale= {0.45}>
+        <div className='root'>
+          <Home></Home><button className="load-button" onClick={this.exportPDFWithComponent}>Lataa PDF-muodossa</button>
+          <Sidebar></Sidebar>
+        </div>
       </PDFExport>
-  );
- }
- exportPDFWithComponent = () => {
-  this.pdfExportComponent.save();
-}
+    );
+  }
+  exportPDFWithComponent = () => {
+    this.pdfExportComponent.save();
+  }
 }
 
 export default App;
