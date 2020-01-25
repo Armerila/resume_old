8d6bb278d9bb5d1df3beb579e1be2d55956c9c2d
Arttu Meriläinen <arkkuvio@gmail.com>
2020-01-25 09:21:20 +0200

    Updates

---
+++ precache-manifest.2ffb150d5004ea5db3f969f4f314f50b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef9f60557c9a9b0b3d2981f2aad8ccc6",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "735d8a282edd90fdf3d2",
+    "url": "/resume/static/css/main.41930742.chunk.css"
+  },
+  {
+    "revision": "b3172ae71f598e2faf15",
+    "url": "/resume/static/js/2.bdb5ff63.chunk.js"
+  },
+  {
+    "revision": "fb76c9f8f8182087f9c180bc5fcbff28",
+    "url": "/resume/static/js/2.bdb5ff63.chunk.js.LICENSE"
+  },
+  {
+    "revision": "735d8a282edd90fdf3d2",
+    "url": "/resume/static/js/main.4f90eee8.chunk.js"
+  },
+  {
+    "revision": "540de6763af4833a1da8",
+    "url": "/resume/static/js/runtime-main.64f02ed5.js"
+  },
+  {
+    "revision": "cefc20232703e5e3c24efd5f50d75e26",
+    "url": "/resume/static/media/GitHub_Logo.cefc2023.png"
+  },
+  {
+    "revision": "c24cde2827448f08d40afd6909011e16",
+    "url": "/resume/static/media/LI-Logo.c24cde28.png"
+  },
+  {
+    "revision": "9a6fb6f5cd3aa4ab1adaaab1f693f266",
+    "url": "/resume/static/media/Rubik-Bold.9a6fb6f5.ttf"
+  },
+  {
+    "revision": "7a06846baf7fd2cfb18c7ab4d663c8ca",
+    "url": "/resume/static/media/Rubik-Light.7a06846b.ttf"
+  },
+  {
+    "revision": "b3d0902b533ff4c4f1698a2f96ddabab",
+    "url": "/resume/static/media/Rubik-Regular.b3d0902b.ttf"
+  },
+  {
+    "revision": "85a1cedf5e32235a9e388e82bcd457c2",
+    "url": "/resume/static/media/koulutus.85a1cedf.png"
+  },
+  {
+    "revision": "20f35dee0590c0f67907d7615ba73095",
+    "url": "/resume/static/media/kurssi.20f35dee.png"
+  },
+  {
+    "revision": "c58e137f08c32922df36cf4dc05c3211",
+    "url": "/resume/static/media/omakuva.c58e137f.jpg"
+  },
+  {
+    "revision": "9752eee32711bf716558999c28109e3e",
+    "url": "/resume/static/media/tyo.9752eee3.png"
+  },
+  {
+    "revision": "63324edc94ecc340ec1633ab60a0f74b",
+    "url": "/resume/static/media/yhteys.63324edc.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Armerila/resume.git/precache-manifest.925fc1ad16d5a970bcf3217aa0b0d028.js"
+  "/resume/precache-manifest.2ffb150d5004ea5db3f969f4f314f50b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Armerila/resume.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
